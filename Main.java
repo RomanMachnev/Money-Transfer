8b9a5f41894ce054838e9cbc57ef543c8ef1e0a2
Roman Machnev <machnev93@yandex.ru>
2021-03-04 12:42:59 +0300

    add main and readme

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int velue1 = 2_000_000_000;
+        int velue2 = 500_000_000 ;
+        int total = velue1 + velue2;
+        System.out.println("Total:" + total);
+    }
+}

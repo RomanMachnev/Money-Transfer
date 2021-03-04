2f2756e38a4a6aaf931238f29c9d34e5da260225
Roman Machnev <machnev93@yandex.ru>
2021-03-04 12:40:35 +0300

    Revert "add gitignore"
    This reverts commit 7c3482c16c3f950dba6be402e65df93a1e180f3a.

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int velue1 = 2_000_000_000;
-        int velue2 = 500_000_000 ;
-        int total = velue1 + velue2;
-        System.out.println("Total:" + total);
-    }
-}
